87683d499efcd7dc4f518db4fefe17574801e0a7
Edward <ing_jorge_caceres@hotmail.com>
2023-04-22 19:38:49 -0400

    commit after repository cloned

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-python.exe -m pip install --upgrade pip
+#python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
