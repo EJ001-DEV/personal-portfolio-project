91ecc215abac999e9eca05b73d62e4a39d217037
Edward <ing_jorge_caceres@hotmail.com>
2023-04-22 20:16:26 -0400

    pip install commented in settings.py

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
-#python.exe -m pip install --upgrade pip
-#python pip install -r requirements.txt
+python.exe -m pip install --upgrade pip
+python pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
