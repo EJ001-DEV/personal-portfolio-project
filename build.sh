1cf50e1d694644901439b973ef4514d5045c4b00
Edward <ing_jorge_caceres@hotmail.com>
2023-04-22 20:18:08 -0400

    pip install commented in settings.py

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-python.exe -m pip install --upgrade pip
+python -m pip install --upgrade pip
 python pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
